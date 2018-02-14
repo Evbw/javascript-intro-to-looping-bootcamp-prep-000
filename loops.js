e31d0b3d69372eda7d8ecdbc3a3f233540409e5e
Everett <everett.bowline@gmail.com>
2018-02-14 00:50:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,9 +18,14 @@ function whileLoop() {
   return "done"
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+} 
+  
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
+  do {
+    
   }
+  while  
 return array
 }
